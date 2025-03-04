93ed85149b6675e0bb2a01223449120e15a7e22b
Hassan Ali <ihasanalyy@gmail.com>
2025-03-04 14:05:47 +0500

    changes

--- utils/geoLocation.js
+++ utils/geoLocation.js
@@ -1,19 +1,44 @@
-import axios from 'axios';
+// import axios from 'axios';
+
+// export const getLocationDetails = async (lat, lng) => {
+//     try {
+//         const response = await axios.get(`https://api.opencagedata.com/geocode/v1/json`, {
+//             params: {
+//                 key: process.env.OPENCAGE_API_KEY,
+//                 q: `${lat},${lng}`,
+//                 language: "en"
+//             }
+//         });
+//         const data = response.data.results[0].components;
+//         return {
+//             country: data.country,
+//             city: data.city || data.town || data.village,
+//             postalCode: data.postcode,
+//         };
+//     } catch (error) {
+//         console.error("Error fetching location data:", error);
+//         return { country: "", city: "", postalCode: "" };
+//     }
+// };
+
+import axios from "axios";
 
 export const getLocationDetails = async (lat, lng) => {
     try {
-        const response = await axios.get(`https://api.opencagedata.com/geocode/v1/json`, {
+        const response = await axios.get(`https://geocode.maps.co/reverse`, {
             params: {
-                key: process.env.OPENCAGE_API_KEY,
-                q: `${lat},${lng}`,
-                language: "en"
+                lat,
+                lon: lng,
+                api_key: process.env.GEOCODE_MAPS_API_KEY // Securely store API key in env
             }
         });
-        const data = response.data.results[0].components;
+
+        const data = response.data.address;
+        console.log(response,"data")
         return {
-            country: data.country,
-            city: data.city || data.town || data.village,
-            postalCode: data.postcode,
+            country: data.country || "",
+            city: data.city || data.state_district || data.village || "",
+            postalCode: data.postcode || "",
         };
     } catch (error) {
         console.error("Error fetching location data:", error);
